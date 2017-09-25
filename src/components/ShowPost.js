cd68cb3ce179d4f0c71672b7f5fe636516a53181
Sam <swkane@indiana.edu>
2017-09-25 14:31:28 -0400

    can read each individual post

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Post extends React.Component {
+  state = {
+    blog: {}
+  }
+
+  componentDidMount() {
+    const { _id } = this.props.match.params;
+    let url = `https://tiny-lasagna-server.herokuapp.com/collections/blogger/${_id}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(blog => {
+      this.setState({blog: blog});
+    });
+  }
+
   render() {
     return (
       <div>
-        This is a post
+        <h4>Title: {this.state.blog.blogTitle}</h4>
+        <h6>Author: {this.state.blog.authorName}</h6>
+        <h5>Entry: {this.state.blog.blogEntry}</h5>
         <Link to='/show'>Back to Blog List</Link>
       </div>
     )
